23f17220068dfeedbea3781ee5854c5586b9bfcb
you <type your email>
2022-10-23 17:29:25 +0530

    first commit

--- src/Project/Phase1/Lockedme.java
+++ src/Project/Phase1/Lockedme.java
@@ -1,3 +1,4 @@
+
 package Project.Phase1;
 
 import java.io.File;
@@ -6,126 +7,105 @@ import java.util.Scanner;
 
 public class Lockedme {
 
-        static void operations(int choose) throws IOException
-{
-    switch(choose)
-  {
-  case 0:
-     {  
-     String path= "E:\\Project-Phase-1\\"; // path where project files are available
-		Scanner sc = new Scanner(System.in);// taking input from the user
-		System.out.println("Path of the file" +path);
-		System.out.println("Displaying the files in this path");
-		File fd=new File(path);//assigning the path to fd1
-		File f2 []=fd.listFiles();// Fetching the list of files from the path
-		// Retrieving the file names in an ascending order(using for-each loop)
-		for(File ff:f2) {
-		     System.out.println( ff.getName());
-		}
-		break;
-		}
-  
-        case 1:// adding new file to path
-      {
-        String path = "D:\\FSD\\Files\\";
-		Scanner sc = new Scanner(System.in);
-		System.out.println("path of file" +path);
-		System.out.println("enter the file name");
-		String filename=sc.next();
-		File f=new File(path+filename);
-		//create/add new file
-		boolean b=f.createNewFile();
-		if(b!=true)
-		{
-		System.out.println("not created");
-		}
-		else
-		 {
-		System.out.println("The File created in the " +path);
-		}
-          break;      
-   
-        }
-    
-        case 2:// deleting a file from the path
-      
-        {
-        String path = "D:\\FSD\\Files\\";
-		Scanner sc = new Scanner(System.in);
-		System.out.println("path of file" +path);
-		System.out.println("enter the file name to delete from this path");
-		String filenames=sc.next();
-		File f1=new File(path+filenames);
-		//create/add new file
-		boolean c=f1.delete();
-				if(c!=true)
-		{
-		System.out.println("not created");
-		}
-		else
-		 {
-		System.out.println("The File created in the " +path);
-		}
-		break;
-        }
-        
-        case 3:// to search a file from given the given path 
-        { 
-        String path = "D:\\FSD\\Files\\";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the file naem to search a particular file");
-		String fs=sc.next();
-		File f3= new File(path);
-		File Fa[]=f3.listFiles();
-		boolean flag = false;
-		// using for each for Iteration
 
-		for(File fr:Fa) {
-				if(fr.getName().equals(fs)) {
-								flag =true;
-		                           break;
-											}
-					else {flag=false;}
-                      }
-				if(flag==true)
-								{
-								System.out.println("file found");
-								}
-					else
-						{System.out.println("file not found");}
-						break;
-        
-	    }
-	    case 4:// exit
-	    {
-	    System.out.println("return to the main context");
-	    }
-	    
-	    case 5:
-	    System.exit(choose);
-}}
+
 	public static void main(String[] args) throws IOException {
-		/*String path= "E:\\Project-Phase-1\\"; // path where project files are available
-		Scanner sc = new Scanner(System.in);// taking input from the user
-		System.out.println("Path of the file" +path);
-		System.out.println("Displaying the files in this path");
-		File fd1=new File(path);//assigning the path to fd1
-		File f2 []=fd1.listFiles();// Fetching the list of files from the path
-		// Retrieving the file names in an ascending order(using for-each loop)
-		for(File ff:f2) {
-			System.out.println( ff.getName());
-		}*/
+		String path = "D:\\Project-1 Virtual Key\\";
+	
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please select the following options to carry out ");
-		System.out.println("0. Retrieving the file names in an ascending order");
-		System.out.println("Please select the Business-level operations");
-		System.out.println("1.Option to add a user specified file to the application");// select this option to add a file
-		System.out.println("2.Option to delete a user specified file from the application");// select this for deleting a file
-        System.out.println("3.Option to search a user specified file from the application");//select this for searching a file
-        System.out.println("4.Navigation option to close the current execution context and return to the main context");////select this for close a file from the current execution context 
 		
-		int select = sc.nextInt();
-		operations(select);
-	}
+			System.out.println("Please select the options to perform 0.display 1.file operations 2.exit ");
+			int choice = sc.nextInt();
 
-}
+			switch(choice) {
+			case 0:
+				System.out.println("Displaying the files in this path" +path);
+				File fd=new File(path);//assigning the path to fd1
+				File f2 []=fd.listFiles();// Fetching the list of files from the path
+				// Retrieving the file names in an ascending order(using for-each loop)
+				for(File ff:f2) {
+					System.out.println( ff.getName());
+				}
+				break;
+			case 1:
+				while(true) {
+				System.out.println(" Select the task to perform 4.add 5.delete 6.search 7.exit");
+				int choice1=sc.nextInt();
+				switch(choice1) {
+				case 4:
+					System.out.println(" Creating / Adding a file in this floder" +path);
+					System.out.println("enter the file name");
+					String filecreate=sc.next();
+					File create=new File(path+filecreate);
+					//create/add new file
+					boolean b=create.createNewFile();
+					if(b!=true)
+					{
+						System.out.println(" Sorry file has not created");
+					}
+					else
+					{
+						System.out.println("The File created in the " +path);
+					}
+					break;   
+
+					// case a
+				case 5:
+					System.out.println(" Deleting a file in this floder" +path);
+					System.out.println("enter the file name");
+					String filedelete=sc.next();
+					File delete=new File(path+filedelete);
+					//create/add new file
+					boolean c=delete.delete();
+					if(c!=true)
+					{
+						System.out.println(" Sorry file has been not deleted");
+					}
+					else
+					{
+						System.out.println("The File has been deleted from the  " +path);
+					}
+					break; 
+
+					//case b
+				case 6:
+					System.out.println("enter the file naem to search a particular file");
+					String filesearch=sc.next();
+					File search=new File(path);
+					File F2[]=search.listFiles();
+					boolean flag = false;
+					// using for each for Iteration
+					for(File fr:F2) {
+						if(fr.getName().equals(filesearch)) {
+							flag =true;
+							break;
+						}
+						else {
+							flag=false;
+							}
+					}
+					if(flag==true)
+					{
+						System.out.println("file found");
+					}
+					else
+					{
+						System.out.println("file not found");
+					}
+					break;//case c
+
+				case 7:
+					System.exit(choice1);
+					
+					
+
+				}// sub switch
+			}// main case1
+			
+			case 2:
+				System.exit(choice);
+				break;
+				
+			}
+		}
+		}
